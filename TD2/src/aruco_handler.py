5a73e7f4efc77fd0afce74ba602e6ee5aca8e1fc
rakinrkz <rakinuzzaman@gmail.com>
2023-07-08 18:12:18 +0600

    minor updates

--- TD2/src/aruco_handler.py
+++ TD2/src/aruco_handler.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python
-
+import time
 import rospy
 import tf2_ros
 from geometry_msgs.msg import Transform
+from utils import Aruco_Marker
 from altair_msgs.srv import ArucoService, ArucoServiceResponse
 
+response_wait = 4.0
 MEMORY = dict()
-
+Aruco_List = [0 for i in range(15)]
 def callback(request):
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
     rate = rospy.Rate(1.0)
-    while not rospy.is_shutdown():
-        for idx in range(1, 15):
-            try:
-                trans = tfBuffer.lookup_transform('base', 'fiducial_' + str(idx), rospy.Time())
-                pos = Transform()
-                # for some reason the transform needs to be negated
-                # except for the z axis?
-                # maybe because z axis direction is same for aruco and camera
-                # but x and y axis direction is opposite of camera
-                pos.translation.x = -trans.transform.translation.x
-                pos.translation.y = -trans.transform.translation.y
-                pos.translation.z =  trans.transform.translation.z
-                pos.rotation = trans.transform.rotation
-                MEMORY[idx] = pos
-            except Exception as e:
-                rate.sleep()
-                print(e)
-    return ArucoServiceResponse(True, request.input)
+    c = 0
+
+    # while not rospy.is_shutdown():
+    for idx in range(request.starting_id, request.ending_id+1):
+        try:
+            trans = tfBuffer.lookup_transform('base_link', 'fiducial_' + str(idx), rospy.Time(), rospy.Duration(response_wait))
+            pos = Transform()
+            # for some reason the transform needs to be negated
+            # except for the z axis?
+            # maybe because z axis direction is same for aruco and camera
+            # but x and y axis direction is opposite of camera
+            detected_aruco = Aruco_Marker()
+            detected_aruco.id = idx
+            detected_aruco.pose = pos
+            pos.translation.x = trans.transform.translation.x
+            pos.translation.y = trans.transform.translation.y
+            pos.translation.z =  trans.transform.translation.z
+            pos.rotation = trans.transform.rotation
+            MEMORY[idx] = pos
+            Aruco_List[idx] = detected_aruco
+            print(c)
+            c+=1
+            print(pos)
+            # time.sleep(0.1)
+            
+        except Exception as e:
+            rate.sleep()
+            print(e)
+    return ArucoServiceResponse((c == (request.ending_id - request.starting_id +1)), c)
 
 def service_runner():
     rospy.init_node('aruco_handler_server')
